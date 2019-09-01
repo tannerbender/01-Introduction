8257b760a040ec85e7526a6e4bd411dada16b748
Tanner Bender <tannerbender@Tanners-MacBook-Pro.local>
2019-08-31 22:31:38 -0400

    Updates introduction files

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Tanner Bender.')
+print('My favorite game to play is Call of Duty')
+print('I am concerned about writing the correct code')
+print('I am excited to create my own video games')
+print('My stackoverflow user number is 11991314')
+print('A link to my github account is https://github.com/tannerbender')
